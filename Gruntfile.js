b56127663a608ab53d39a48109fc0c51d3063985
Martin Michálek <michalek@shortcat.cz>
2018-05-16 09:13:46 +0200

    Prettier Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,80 +1,66 @@
 module.exports = function(grunt) {
-
   // Nacteme vsechny grunt tasky a urychlime srandu
-  require('jit-grunt')(grunt);
+  require("jit-grunt")(grunt);
 
   grunt.initConfig({
-
     less: {
       default: {
         files: {
-          'css/style.css': 'less/index.less'
+          "css/style.css": "less/index.less"
         },
         options: {
           sourceMap: true,
-          sourceMapFilename: 'css/style.css.map',
-          sourceMapURL: 'rekrea.css.map',
-          sourceMapRootpath: ''
+          sourceMapFilename: "css/style.css.map",
+          sourceMapURL: "rekrea.css.map",
+          sourceMapRootpath: ""
         }
       }
     },
 
     postcss: {
       options: {
-        processors: [
-          require('autoprefixer')()
-        ]
+        processors: [require("autoprefixer")()]
       },
       default: {
-        src: [
-          'css/style.css'
-        ]
-      }    
+        src: ["css/style.css"]
+      }
     },
 
     browserSync: {
       default: {
         bsFiles: {
-            src : [
-              'css/style.css'
-            ]
+          src: ["css/style.css"]
         },
-        options: {            
-            watchTask: true,
-            server: {
-                baseDir: "./"
-            }            
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: "./"
+          }
         }
       }
     },
 
     watch: {
       default: {
-        files: [
-          'less/**/*.less'
-        ],
-        tasks: ['css']
+        files: ["less/**/*.less"],
+        tasks: ["css"]
       }
     },
 
     stylelint: {
       options: {
-        configFile: '.stylelintrc',
-        formatter: 'string',
+        configFile: ".stylelintrc",
+        formatter: "string",
         ignoreDisables: false,
         failOnError: true,
-        outputFile: '',
+        outputFile: "",
         reportNeedlessDisables: false,
-        syntax: 'less'
+        syntax: "less"
       },
-      src: [
-        'less/**/*.less'
-      ]
+      src: ["less/**/*.less"]
     }
-
   });
 
-  grunt.registerTask('css', ['less', 'postcss']);
-  grunt.registerTask('default', ['browserSync', 'watch']);
-
+  grunt.registerTask("css", ["less", "postcss"]);
+  grunt.registerTask("default", ["browserSync", "watch"]);
 };
